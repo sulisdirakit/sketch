33e1abb6ca7330eee16b15df7fe8f3f0188fd662
sulisdirakit <sulis@dirakit.com>
2016-11-15 16:56:48 +0700

    Add files via upload

---
+++ esp8266-12_blink.ino
@@ -0,0 +1,19 @@
+int gpioCount=8;
+int led_pin[8]={0,2,4,12,13,14,15,16};
+
+void setup() {
+  for (byte i=0; i<gpioCount; i++) {
+     pinMode(led_pin[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (byte i=0; i<gpioCount; i++) {
+     digitalWrite(led_pin[i], LOW); //turn LED on, it is acive low on the ESP-07
+     delay(50);  
+     digitalWrite(led_pin[i], HIGH); 
+     delay(50);  
+     digitalWrite(led_pin[i], LOW); //turn LED on, it is acive low on the ESP-07
+     delay(50);
+  }
+}
